eb3f7e0690cb9610f3b3eaafeba487ec21a572e7
alexey02rus <101921891+alexey02rus@users.noreply.github.com>
2022-07-31 22:07:50 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\YandexDisk\Курс ИТМО\СSharp";
+            string[] dires = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+            string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            Console.WriteLine("Папки и подпапки:");
+            foreach (string dir in dires)
+            {
+                Console.WriteLine(dir);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Файлы во всех папках и подпапках:");
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\ASUS ROG\Desktop\numbers.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            int i = 0;
+            int j = 0;
+            int length1 = 0;
+            int length2 = 0;
+            int count = 0;
+            string str;
+            Console.WriteLine($"Имя файла: {fileInfo.Name}");
+            Console.WriteLine($"Полный путь до файла: {fileInfo.DirectoryName}");
+            Console.WriteLine($"Время создания: {fileInfo.CreationTime}");
+            Console.WriteLine($"Размер: {fileInfo.Length} байт");
+            Console.WriteLine();
+            Console.WriteLine("Статистика по файлу:");;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                
+                while ((str = sr.ReadLine()) != null)
+                {
+                    i++; // Кол-во строк
+                    if (str.Replace(" ","") != "")
+                    {
+                        j++; // Кол-во не пустых строк
+                        length1 += str.Length; // Кол-во симоволов с учетом пробелов
+                        length2 += str.Replace(" ", "").Length; // Кол-во симоволов без учета пробелов
+                        count += str.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Length; // Кол-во слов в строке
+                    }
+
+                }
+
+            }
+            Console.WriteLine("Количество строк в тексте (с учетом пустых): {0}", i);
+            Console.WriteLine("Количество строк в тексте (без учета пустых): {0}", j);
+            Console.WriteLine("Количество слов в тексте: {0}", count);
+            Console.WriteLine("Количество символов в тексте (с учетом пробелов): {0}", length1);
+            Console.WriteLine("Количество символов в тексте (без учета пробелов): {0}", length2);
+            Console.ReadKey();
+        }
+    }
+}
